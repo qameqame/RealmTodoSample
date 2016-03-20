21ccd8d654e2aa3b4d94f5896549855128d505f7
hkameyam <hkameyam@gmail.com>
2016-03-20 15:15:59 +0900

    Initial Commit

---
+++ RealmTest2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RealmTest2
+//
+//  Created by 亀山裕樹 on 2016/03/20.
+//  Copyright © 2016年 Hiroki Kameyama. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
